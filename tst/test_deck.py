ad55468ee33a5b7c1e302910612b1fd943a24978
Charles Cox <chcox313@gmail.com>
2016-12-29 11:52:31 -0800

    Add new unittest helper submodule and change to python 2.7 version of tests

--- tst/test_deck.py
+++ tst/test_deck.py
@@ -1,7 +1,7 @@
 from Deck.deck import Deck
 from Deck.card import Card
 from Deck.exceptions import DeckSizeException
-
+import UnitTestHelper.helper_functions as helper
 
 def build_sample_deck(seed=None):
     names = ['First Card', 'Second Card', 'Third Card']
@@ -24,23 +24,21 @@ def test_draw_too_many_cards():
 
 def test_draw_cards():
     deck = build_sample_deck(10)
-    deck.add_cards(build_sample_deck())
     cards = deck.draw_cards(2)
-    ref_names = ['Third Card', 'First Card']
+    ref_names = ['Second Card', 'First Card']
     for card, name in zip(cards, ref_names):
-        if card.name != name:
-            print('Unexpected card drawn: %s expected: %s' % (card.name, name))
-            assert False
+        helper.print_if_assert('Unexpected card', card.name, name)
 
 
 def test_draw_card_reshuffle():
     deck = build_sample_deck(10)
     deck.reshuffle = True
     cards = deck.draw_cards(5)
-    ref_names = ['Third Card', 'First Card', 'Second Card', 'Second Card', 'First Card']
-    for card, name in zip(cards, ref_names):
+    ref_names = ['Second Card', 'First Card', 'Third Card', 'First Card', 'Second Card']
+    for ind, pair in enumerate(zip(cards, ref_names)):
+        card, name = pair
         if card.name != name:
-            print('Unexpected card drawn: %s expected: %s' % (card.name, name))
+            print('Unexpected card pos: %s drawn %s expected: %s' % (ind, card.name, name))
             assert False
 
 def test_grab_splits_fail():
@@ -54,3 +52,5 @@ def test_grab_splits():
     deck = build_sample_deck(10)
     first, second = deck.grab_splits(2, False)
     ref_first, ref_second = 2, 0
+    helper.print_if_assert('First split', first, ref_first)
+    helper.print_if_assert('Second split', second, ref_second)
