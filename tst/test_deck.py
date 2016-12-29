4bab4d2c8888b0c511c0c7856887c071e738a3df
Charles Cox <chcox313@gmail.com>
2016-12-29 11:31:36 -0800

    Minor refactor of test deck construction

--- tst/test_deck.py
+++ tst/test_deck.py
@@ -3,15 +3,16 @@ from Deck.card import Card
 from Deck.exceptions import DeckSizeException
 
 
-def build_sample_deck():
+def build_sample_deck(seed=None):
     names = ['First Card', 'Second Card', 'Third Card']
     cards = [Card(name) for name in names]
-    return cards
+    deck = Deck('Test Deck', seed=seed)
+    deck.add_cards(cards)
+    return deck
 
 
 def test_draw_too_many_cards():
-    deck = Deck('Test Deck')
-    deck.add_cards(build_sample_deck())
+    deck = build_sample_deck()
     try:
         deck.draw_cards(10)
     except DeckSizeException:
@@ -22,7 +23,7 @@ def test_draw_too_many_cards():
 
 
 def test_draw_cards():
-    deck = Deck('Test Deck', seed=10)
+    deck = build_sample_deck(10)
     deck.add_cards(build_sample_deck())
     cards = deck.draw_cards(2)
     ref_names = ['Third Card', 'First Card']
@@ -33,8 +34,7 @@ def test_draw_cards():
 
 
 def test_draw_card_reshuffle():
-    deck = Deck('Test Deck', seed=10)
-    deck.add_cards(build_sample_deck())
+    deck = build_sample_deck(10)
     deck.reshuffle = True
     cards = deck.draw_cards(5)
     ref_names = ['Third Card', 'First Card', 'Second Card', 'Second Card', 'First Card']
@@ -42,3 +42,15 @@ def test_draw_card_reshuffle():
         if card.name != name:
             print('Unexpected card drawn: %s expected: %s' % (card.name, name))
             assert False
+
+def test_grab_splits_fail():
+    deck = build_sample_deck(10)
+    try:
+        _, _ = deck.grab_splits(5, False)
+    except DeckSizeException:
+        pass
+
+def test_grab_splits():
+    deck = build_sample_deck(10)
+    first, second = deck.grab_splits(2, False)
+    ref_first, ref_second = 2, 0
