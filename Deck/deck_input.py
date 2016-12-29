d481d7586c072c73ec10219173bc0fc5164c592c
David Cox <davidjeremycox@gmail.com>
2016-12-28 16:50:47 -0800

    Adding working deck_input for basic decks

--- Deck/deck_input.py
+++ Deck/deck_input.py
@@ -1,22 +1,38 @@
 import json
 from card import Card
+from Deck.deck import Deck
 """ This encapsulates the needed input to read and create a deck -- aka a list of cards from a file """
+
+
+DEFAULT_NAME = 'DefaultDeck'
 
 
 def build_card(a_definition):
     pass
 
 
-def build_card_definitions(json):
+def build_card_definitions(json_input):
     definitions = []
-    if 'card_definitions' in json.keys():
-        for a_definition in json['card_definitions']:
+    if 'card_definitions' in json_input.keys():
+        for a_definition in json_input['card_definitions']:
             definitions.append(build_card(a_definition))
     return definitions
 
 
-def get_cards(json, card_definitions):
+def card_with_definition(a_card, all_defintions):
     pass
+
+
+def get_cards(json_input, card_definitions):
+    cards = []
+    if 'cards' in json_input.keys():
+        for card_name in sorted(json_input['cards'].iterkeys()):
+            card_value = json_input['cards'][card_name]
+            if isinstance(card_value, dict):
+                cards.append(card_with_definition(card_value, card_definitions))
+            else:
+                cards.extend([Card(card_name)]*int(card_value))
+    return cards
 
 
 def build_cards(json):
@@ -25,11 +41,22 @@ def build_cards(json):
     return cards
 
 
-def read_file(filename):
-    output_list = []
-    with open()
-    raw = json.loads(filename)
-    output_list.extend(build_cards(raw))
+def get_with_default(json_input, param, default_value):
+    if param in json_input:
+        return json_input[param]
+    else:
+        return default_value
 
-    return output_list
+
+def read_file(filename, override_seed=None):
+    with open(filename, 'r') as in_file:
+        raw = json.load(in_file)
+    cards = build_cards(raw)
+    name = get_with_default(raw, 'deckName', DEFAULT_NAME)
+    seed = get_with_default(raw, 'seed', override_seed)
+    deck = Deck(name=name, seed=seed)
+    for a_card in sorted(cards):
+        deck.add_card(a_card)
+
+    return deck
 
--- tst/test_deck_input.py
+++ tst/test_deck_input.py
@@ -1,9 +1,15 @@
-from Deck import deck_input
 from Deck.card import Card
+from Deck.deck import Deck
+import Deck.deck_input as deck_input
 
 
 def test_read_file():
-    test_filepath = 'Inputs/test_deck_definition.json'
+    test_filepath = 'tst/Inputs/test_deck_definition.json'
     test_output = deck_input.read_file(test_filepath)
-    expected_out = [Card('card1')*10, Card('card2')*5]
-    assert(test_output == expected_out)
+    expected_cards = [Card(u'card1')]*10
+    expected_cards.extend([Card(u'card2')]*5)
+    deck = Deck(name=deck_input.DEFAULT_NAME, seed=None)
+    for a_card in sorted(expected_cards):
+        deck.add_card(a_card)
+    for test_card, expected_card in zip(test_output.card_list, deck.card_list):
+        assert(test_card == expected_card)
