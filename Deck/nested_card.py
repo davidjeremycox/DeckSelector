bfcc77121a166bbf82ead56ddcb645b9da2608fa
Charles Cox <chcox313@gmail.com>
2017-04-22 09:10:39 -0700

    Correct failure to properly define replace_draws

--- Deck/nested_card.py
+++ Deck/nested_card.py
@@ -7,6 +7,7 @@ class NestedCard(Card):
 
     def __init__(self, in_name, json_values=None, card_definitions=None, deck=None, replace_draws=None):
         super(NestedCard, self).__init__(in_name=in_name)
+        self.replace_draws = replace_draws
         if json_values is not None:
             cards = deck_input.get_cards(json_values, card_definitions)
             deck_name = deck_input.get_with_default(json_values, deck_input.DECK_NAME_FIELD_NAME, deck_input.DEFAULT_NAME)
@@ -18,8 +19,6 @@ class NestedCard(Card):
             self.deck.add_cards(cards)
         elif deck is not None:
             self.deck = deck
-            if replace_draws:
-                self.replace_draws = replace_draws
         else:
             raise NotImplementedError("Unable to construct nested card without deck or json values")
 
