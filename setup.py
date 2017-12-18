b2de2b18a113b1f92634b043b84fcf53edf164b7
davidjeremycox <davidjeremycox@gmail.com>
2017-12-17 18:32:58 -0800

    Adding a setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='DeckSelector',
+      version='1.0',
+      description='Drawing cards from a deck',
+      author='David Cox',
+      author_email='davidjeremycox@gmail.com',
+      packages=['Deck'],
+     )
